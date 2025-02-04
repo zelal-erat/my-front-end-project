804208ff47295cf84baacd33804129c827235ca3
zelal-erat <zelaleratt@gmail.com>
2025-02-04 20:57:08 +0300

    commit

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,7 +7,7 @@ export default function HeroSection() {
   return (
     <div className="flex items-center justify-center h-screen ml-[70px] ">
       <div className="w-1/2">
-        <h2 className="text-[60px] mb-5 font-bold">{translations.heroSection.intro}</h2> {/* Dinamik intro metni */}
+        <h2 className="text-[60px] mb-5 font-bold">{translations.heroSection?.intro}</h2> {/* Dinamik intro metni */}
         <p className="text-[1.2rem]" >{translations.heroSection.description}</p> {/* Dinamik açıklama metni */}
         <div className="flex flex-row items-center gap-4 text-blue-500">
   <button className="flex items-center p-3 bg-blue-500  rounded hover:bg-blue-500">
