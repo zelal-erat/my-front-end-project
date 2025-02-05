ee9990632d8e5fab35e3d3577863f1d44df1c250
zelal-erat <zelaleratt@gmail.com>
2025-02-05 16:16:02 +0300

    commit

--- src/components/ProfileSection.jsx
+++ src/components/ProfileSection.jsx
@@ -1,33 +1,30 @@
-
 import { useContext } from "react";
 import { SettingsContext } from "../contexts/SettingsContext";
 
 export default function ProfileSection() {
-  const { translations } = useContext(SettingsContext); // translations'ı context'ten al
+  const { translations } = useContext(SettingsContext);
+  const { title, personalInfo, aboutMe } = translations?.profileSection || {};
 
   return (
-    <div className="ml-[70px] mr-[70px] mt-[70px] ">
-      <h1 className="font-bold text-4xl mb-[20px]">{translations?.profileSection?.title}</h1> {/* Dinamik profil başlığı */}
+    <div className="ml-[70px] mr-[70px] mt-[70px]">
+      <h1 className="font-bold text-4xl mb-[20px]">{title}</h1>
 
       <div className="flex justify-between">
-
-      <div className="">
-        <h2 className=" text-[#4338CA] font-medium text-xl mb-[10px]">{translations?.profileSection?.title}</h2> {/* Dinamik profil başlığı */}
-        <ul>
-  {translations?.profileSection?.personalInfo.map((info, index) => (
-    <li key={index} className="flex items-center mb-2">
-      <label className="font-bold mr-4 w-36">{info.label}:</label>  {/* Sabit genişlik */}
-      <span>{info.value}</span>
-    </li>
-  ))}
-</ul>
-
-      </div>
-
-      <div className="w-[50%]">
-        <h2 className=" text-[#4338CA] font-medium text-xl mb-[10px]"> {translations?.profileSection?.aboutMe?.title}</h2> {/* Dinamik Hakkımda başlığı */}
-        <p className="text-[#6B7280]" >{translations?.profileSection?.aboutMe.content}</p> {/* Dinamik Hakkımda içeriği */}
-      </div>
+        {[ 
+          { sectionTitle: title, content: personalInfo?.map(({ label, value }, index) => (
+              <li key={index} className="flex items-center mb-2">
+                <label className="font-bold mr-4 w-36">{label}:</label>
+                <span>{value}</span>
+              </li>
+            )) 
+          },
+          { sectionTitle: aboutMe?.title, content: <p className="text-[#6B7280]">{aboutMe?.content}</p> }
+        ].map(({ sectionTitle, content }, index) => (
+          <div key={index} className={index === 0 ? "" : "w-[50%]"}>
+            <h2 className="text-[#4338CA] font-medium text-xl mb-[10px]">{sectionTitle}</h2>
+            {Array.isArray(content) ? <ul>{content}</ul> : content}
+          </div>
+        ))}
       </div>
     </div>
   );
